720eb4bc0eed492f5135adcc6750bd691a7213ba
JohnJiang900526 <jiangangshjd@163.com>
2024-01-10 21:26:22 +0800

    添加阻尼模拟

--- src/pages/model-list/detail-01/002-backdrop-area/tools.ts
+++ src/pages/model-list/detail-01/002-backdrop-area/tools.ts
@@ -103,6 +103,7 @@ export class Model {
     // 控制器
     this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
     this.controls.target.set(0, 1, 0);
+    this.controls.enableDamping = true;
     this.controls.update();
 
     this.setGUI();
