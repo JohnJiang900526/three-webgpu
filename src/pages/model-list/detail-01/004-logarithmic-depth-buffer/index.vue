da97880bc69b7df3e9ac3f7be68b3801013d73a1
JohnJiang900526 <jiangangshjd@163.com>
2024-11-13 17:25:51 +0800

    案例 004-logarithmic-depth-buffer  初始化完成

--- src/pages/model-list/detail-01/004-logarithmic-depth-buffer/index.vue
+++ src/pages/model-list/detail-01/004-logarithmic-depth-buffer/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="webgpu-page">
-    <div ref="container" class="page-inner"></div>
+    <div ref="container" class="page-inner">
+      <div ref="containerNormal" class="container-normal">
+        <h2 class="renderer_label">normal z-buffer</h2>
+      </div>
+			<div ref="containerLogzbuf" class="container-logzbuf">
+        <h2 class="renderer_label">logarithmic z-buffer</h2>
+      </div>
+      <div ref="rendererBorder" class="renderer-border"></div>
+    </div>
   </div>
 </template>
 
@@ -10,9 +18,16 @@ import { Model } from "./tools";
 
 let objModel: Model | null = null;
 const container = ref<HTMLDivElement>(document.createElement("div"));
+const containerNormal = ref<HTMLDivElement>(document.createElement("div"));
+const containerLogzbuf = ref<HTMLDivElement>(document.createElement("div"));
+const rendererBorder = ref<HTMLDivElement>(document.createElement("div"));
 
 onMounted(() => {
-  objModel = new Model(container.value);
+  objModel = new Model(container.value, {
+    border: rendererBorder.value,
+    normal: containerNormal.value,
+    logzbuf: containerLogzbuf.value,
+  });
   objModel?.init();
 });
 
@@ -35,6 +50,40 @@ onBeforeUnmount(() => {
         top: 0px;
         right: 0px;
       }
+      .container-normal {
+				width: 25%;
+        height: 100%;
+				display: inline-block;
+				position: relative;
+			}
+
+			.container-logzbuf {
+				width: 75%;
+        height: 100%;
+				display: inline-block;
+				position: relative;
+			}
+      .renderer-border {
+        position: absolute;
+				top: 0;
+				left: 25%;
+				bottom: 0;
+				width: 2px;
+				z-index: 10;
+				opacity: 0.8;
+				background: #ccc;
+				border: 1px inset #ccc;
+				cursor: col-resize;
+      }
+      .renderer_label {
+				position: absolute;
+				bottom: 1em;
+				width: 100%;
+				color: white;
+				z-index: 10;
+				display: block;
+				text-align: center;
+			}
     }
   }
 </style>
--- src/pages/model-list/detail-01/004-logarithmic-depth-buffer/tools.ts
+++ src/pages/model-list/detail-01/004-logarithmic-depth-buffer/tools.ts
@@ -2,7 +2,14 @@ import * as THREE from 'three';
 import GUI from 'lil-gui';
 import Stats from 'stats.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import WebGPU from '@/common/jsm/capabilities/WebGPU.js';
+import WebGL from '@/common/jsm/capabilities/WebGL.js';
+import WebGPURenderer from '@/common/jsm/renderers/webgpu/WebGPURenderer.js';
 
+
+let func = () => {};
 export class Model {
   private width: number;
   private height: number;
@@ -15,8 +22,25 @@ export class Model {
   private animateNumber: number;
 
   private controls: null | OrbitControls;
+  private NEAR: number;
+  private FAR: number;
+  private screensplit: number;
+  private screensplit_right: number;
+  private mouse: number[];
+  private zoompos: number;
+  private minzoomspeed: number;
+  private zoomspeed: number;
+  private border: HTMLDivElement;
+  private normal: HTMLDivElement;
+  private logzbuf: HTMLDivElement;
+  private objects: {[key: string]: any};
+  private labeldata: {size: number, scale: number, label: string}[];
   private gui: GUI;
-  constructor(container: HTMLDivElement) {
+  constructor(container: HTMLDivElement, option: {
+    normal: HTMLDivElement,
+    border: HTMLDivElement,
+    logzbuf: HTMLDivElement,
+  }) {
     this.container = container;
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
@@ -33,43 +57,139 @@ export class Model {
       autoPlace: false,
       container: this.container,
     });
-    this.gui.hide();
+    this.NEAR = 1e-6;
+    this.FAR = 1e27;
+    this.screensplit = 0.25
+    this.screensplit_right = 0;
+    this.mouse = [0.5, 0.5];
+    this.zoompos = -100;
+    this.minzoomspeed = 0.015
+    this.zoomspeed = this.minzoomspeed;
+    this.border = option.border;
+    this.normal = option.normal;
+    this.logzbuf = option.logzbuf;
+    this.objects = {};
+    this.labeldata = [
+      { size: .01, scale: 0.0001, label: 'microscopic (1µm)' },
+      { size: .01, scale: 0.1, label: 'minuscule (1mm)' },
+      { size: .01, scale: 1.0, label: 'tiny (1cm)' },
+      { size: 1, scale: 1.0, label: 'child-sized (1m)' },
+      { size: 10, scale: 1.0, label: 'tree-sized (10m)' },
+      { size: 100, scale: 1.0, label: 'building-sized (100m)' },
+      { size: 1000, scale: 1.0, label: 'medium (1km)' },
+      { size: 10000, scale: 1.0, label: 'city-sized (10km)' },
+      { size: 3400000, scale: 1.0, label: 'moon-sized (3,400 Km)' },
+      { size: 12000000, scale: 1.0, label: 'planet-sized (12,000 km)' },
+      { size: 1400000000, scale: 1.0, label: 'sun-sized (1,400,000 km)' },
+      { size: 7.47e12, scale: 1.0, label: 'solar system-sized (50Au)' },
+      { size: 9.4605284e15, scale: 1.0, label: 'gargantuan (1 light year)' },
+      { size: 3.08567758e16, scale: 1.0, label: 'ludicrous (1 parsec)' },
+      { size: 1e19, scale: 1.0, label: 'mind boggling (1000 light years)' }
+    ];
   }
 
   init() {
-    
-    // 场景
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x333333);
+    if (WebGPU.isAvailable() === false && WebGL.isWebGL2Available() === false) {
+      throw new Error('No WebGPU or WebGL2 support');
+    }
 
-    // 相机
-    this.camera = new THREE.PerspectiveCamera(70, this.aspect, 1, 3500);
-    this.camera.position.z = 4;
-
-    // 渲染器
-    this.createRenderer();
-
-    // 控制器
-    this.controls = new OrbitControls(this.camera, this.renderer?.domElement);
-    this.controls.update();
+    this.loadFont();
 
     this.initStats();
-    this.animate();
-    this.resize();
+  }
+
+  private loadFont() {
+    const loader = new FontLoader();
+    loader.load('/examples/fonts/helvetiker_regular.typeface.json', (font) => {
+      const scene = this.initScene(font);
+      this.objects.normal = this.initView(scene, 'normal', false);
+      this.objects.logzbuf = this.initView(scene, 'logzbuf', true);
+
+      this.bind();
+    });
+  }
+
+  private initScene(font: any) {
+    const scene = new THREE.Scene();
+    scene.add(new THREE.AmbientLight(0x777777));
+
+    const light = new THREE.DirectionalLight(0xffffff, 3);
+    light.position.set(100, 100, 100);
+    scene.add(light);
+
+    const materialargs = {
+      color: 0xffffff as (number | THREE.Color),
+      specular: 0x050505,
+      shininess: 50,
+      emissive: 0x000000
+    };
+
+    const geometry = new THREE.SphereGeometry(0.5, 24, 12);
+
+    for (let i = 0; i < this.labeldata.length; i++) {
+      const scale = this.labeldata[i].scale || 1;
+      const labelgeo = new TextGeometry(this.labeldata[i].label, {
+        font: font,
+        size: this.labeldata[i].size,
+        height: this.labeldata[i].size / 2
+      });
+
+      labelgeo.computeBoundingSphere();
+      if (labelgeo.boundingSphere) {
+        labelgeo.translate(-labelgeo.boundingSphere.radius, 0, 0);
+      }
+      materialargs.color = new THREE.Color().setHSL(Math.random(), 0.5, 0.5);
+
+      const material = new THREE.MeshPhongMaterial(materialargs);
+      const group = new THREE.Group();
+      group.position.z = - this.labeldata[i].size * scale;
+      scene.add(group);
+
+      const textmesh = new THREE.Mesh(labelgeo, material);
+      textmesh.scale.set(scale, scale, scale);
+      textmesh.position.z = - this.labeldata[i].size * scale;
+      textmesh.position.y = this.labeldata[i].size / 4 * scale;
+      group.add(textmesh);
+
+      const dotmesh = new THREE.Mesh(geometry, material);
+      dotmesh.position.y = - this.labeldata[i].size / 4 * scale;
+      dotmesh.scale.multiplyScalar(this.labeldata[i].size * scale);
+      group.add(dotmesh);
+    }
+
+    return scene;
+  }
+
+  private initView(scene: THREE.Scene, name: "normal" | "logzbuf", logDepthBuf: boolean) {
+    const framecontainer = this[name];
+
+    const aspect = (this.screensplit * this.width / this.height);
+    const camera = new THREE.PerspectiveCamera(50, aspect, this.NEAR, this.FAR);
+    scene.add(camera);
+
+    const renderer = new WebGPURenderer({ 
+      antialias: true, 
+      logarithmicDepthBuffer: logDepthBuf 
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(this.width / 2, this.height);
+    renderer.setAnimationLoop(() => { this.render(); });
+    renderer.domElement.style.position = 'relative';
+    renderer.domElement.id = 'renderer_' + name;
+    framecontainer.appendChild(renderer.domElement);
+
+    return { 
+      container: framecontainer, 
+      renderer: renderer, 
+      scene: scene, 
+      camera: camera 
+    };
   }
 
   // 判断是否为移动端
   isMobile() {
     const userAgent = window.navigator.userAgent.toLowerCase();
     return userAgent.includes("mobile");
-  }
-
-  // 创建渲染器
-  private createRenderer() {
-    this.renderer = new THREE.WebGLRenderer({antialias: true});
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(this.width, this.height);
-    this.container.appendChild(this.renderer.domElement);
   }
 
   // 性能统计
@@ -86,39 +206,106 @@ export class Model {
     this.container.appendChild(this.stats.domElement);
   }
 
-  // 持续动画
-  private animate() {
-    this.animateNumber && window.cancelAnimationFrame(this.animateNumber);
-    this.animateNumber = window.requestAnimationFrame(() => { this.animate(); });
+  private render() {
+    const minzoom = this.labeldata[0].size * this.labeldata[0].scale * 1;
+    const maxzoom = this.labeldata[this.labeldata.length - 1].size * this.labeldata[this.labeldata.length - 1].scale * 100;
+    let damping = (Math.abs(this.zoomspeed) > this.minzoomspeed ? .95 : 1.0);
+
+    // Zoom out faster the further out you go
+    const zoom = THREE.MathUtils.clamp(Math.pow(Math.E, this.zoompos), minzoom, maxzoom);
+    this.zoompos = Math.log(zoom);
+
+    // Slow down quickly at the zoom limits
+    if ((zoom == minzoom && this.zoomspeed < 0) || (zoom == maxzoom && this.zoomspeed > 0)) {
+      damping = 0.85;
+    }
+
+    this.zoompos += this.zoomspeed;
+    this.zoomspeed *= damping;
+
+    this.objects.normal.camera.position.x = Math.sin(.5 * Math.PI * (this.mouse[0] - .5)) * zoom;
+    this.objects.normal.camera.position.y = Math.sin(.25 * Math.PI * (this.mouse[1] - .5)) * zoom;
+    this.objects.normal.camera.position.z = Math.cos(.5 * Math.PI * (this.mouse[0] - .5)) * zoom;
+    this.objects.normal.camera.lookAt(this.objects.normal.scene.position);
+
+    // Clone camera settings across both scenes
+    this.objects.logzbuf.camera.position.copy(this.objects.normal.camera.position);
+    this.objects.logzbuf.camera.quaternion.copy(this.objects.normal.camera.quaternion);
+
+    // Update renderer sizes if the split has changed
+    if (this.screensplit_right != 1 - this.screensplit) {
+      this.resizeHandle();
+    }
+
+    this.objects.normal.renderer.render(this.objects.normal.scene, this.objects.normal.camera);
+    this.objects.logzbuf.renderer.render(this.objects.logzbuf.scene, this.objects.logzbuf.camera);
 
     this.stats?.update();
-    this.controls?.update();
-
-    // 执行渲染
-    this.renderer?.render(this.scene, this.camera!);
   }
 
   private resizeHandle() {
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
-    this.aspect = this.width/this.height;
+    this.aspect = this.width / this.height;
 
-    this.camera!.aspect = this.aspect;
-    // 更新摄像机投影矩阵。在任何参数被改变以后必须被调用。
-    this.camera!.updateProjectionMatrix();
+    this.screensplit_right = 1 - this.screensplit;
 
-    this.renderer!.setSize(this.width, this.height);
+    this.objects.normal.renderer.setSize(this.screensplit * this.width, this.height);
+    this.objects.normal.camera.aspect = this.screensplit * this.width / this.height;
+    this.objects.normal.camera.updateProjectionMatrix();
+    this.objects.normal.camera.setViewOffset(this.width, this.height, 0, 0, this.width * this.screensplit, this.height);
+    this.objects.normal.container.style.width = (this.screensplit * 100) + '%';
+
+    this.objects.logzbuf.renderer.setSize(this.screensplit_right * this.width, this.height);
+    this.objects.logzbuf.camera.aspect = this.screensplit_right * this.width / this.height;
+    this.objects.logzbuf.camera.updateProjectionMatrix();
+    this.objects.logzbuf.camera.setViewOffset(this.width, this.height, this.width * this.screensplit, 0, this.width * this.screensplit_right, this.height);
+    this.objects.logzbuf.container.style.width = (this.screensplit_right * 100) + '%';
+
+    this.border.style.left = (this.screensplit * 100) + '%';
   }
 
   // 消除 副作用
   dispose() {
     window.cancelAnimationFrame(this.animateNumber);
-    window.removeEventListener("resize", this.resizeHandle);
+    window.removeEventListener("resize", func);
+
+    window.onwheel = null;
+    window.onmousemove = null;
+    window.onpointerdown = null;
   }
 
   // 处理自适应
   resize() {
-    window.addEventListener("resize", this.resizeHandle);
+    func = () => { this.resizeHandle };
+    window.addEventListener("resize", func);
+  }
+
+  private bind() {
+    window.onpointerdown = () => {
+      window.onpointermove = (e) => {
+        this.screensplit = Math.max(0, Math.min(1, e.clientX / this.width));
+      };
+
+      window.onpointerup = () => {
+        window.onpointermove = null;
+        window.onpointerup = null;
+      };
+    };
+
+    window.onmousemove = (e) => {
+      this.mouse[0] = e.clientX / this.width;
+      this.mouse[1] = e.clientY / this.height;
+    };
+
+    window.onwheel = (e) => {
+      const amount = e.deltaY;
+      if (amount === 0) { return; }
+
+      const dir = amount / Math.abs(amount);
+      this.zoomspeed = dir / 10;
+      this.minzoomspeed = 0.001;
+    };
   }
 }
 
