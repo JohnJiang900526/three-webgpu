953db7fb823f9328fd3b7fa9b9a3ed3acf122d46
JohnJiang900526 <jiangangshjd@163.com>
2025-01-23 16:14:20 +0800

    文件模块添加

---
+++ src/pages/model-list/detail-02/018-instance-mesh/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="webgpu-page">
+    <div ref="container" class="page-inner"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { Model } from "./tools";
+
+let objModel: Model | null = null;
+const container = ref<HTMLDivElement>(document.createElement("div"));
+
+onMounted(() => {
+  objModel = new Model(container.value);
+  objModel?.init();
+});
+
+onBeforeUnmount(() => {
+  objModel?.dispose();
+  objModel = null;
+});
+</script>
+<style lang='less'>
+  @import "@/common/style/color.less";
+  @import "@/common/style/mixins.less";
+
+  .webgpu-page {
+    .absolute-page();
+    .page-inner {
+      position: relative;
+      .width-and-height();
+      .lil-gui.root {
+        position: absolute;
+        top: 0px;
+        right: 0px;
+      }
+    }
+  }
+</style>
+
